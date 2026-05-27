8f2a70c27c359ec0e89f6a62cdf1f60d23d02af1
chuguowei <chuguowei@stu.ahu.edu.cn>
2026-05-27 18:27:57 +0800

    Delete CAST directory

--- CAST/config.py
+++
@@ -1,3 +0,0 @@
-CHOICE_DATASETS   = ['aqua', 'CSQA', 'bigbench_date', 'object_tracking']
-QUESTION_DATASETS = ['addsub', 'GSM8K', 'singleeq', 'MultiArith', 'SVAMP']
-YESNO_DATASETS    = ['Strategy']
